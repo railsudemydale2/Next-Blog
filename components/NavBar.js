ddb3d5c5a07052a3c6f7791d71b835bcb799e74b
dale <stewartdale45@yahoo.com>
2021-09-11 21:02:12 -0400

    Implemented Component Styles with Styled JSX

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,24 +1,35 @@
 import Link from 'next/link';
 
 function NavBar() {
-    return (
-        <nav>
-          <ul>
-              <li>
-                <Link href="/">
-                    <a>Home</a>
-                </Link>
-              
-            </li>
-            <li>
-                <Link href="/about">
-                    <a>About</a>
-                </Link>
-              
-            </li>
-          </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+      </ul>
+      <style jsx>{`
+        ul {
+          list-style-type: none;
+          padding: 0;
+        }
+        li {
+          display: inline;
+        }
+
+        li:not(:first-child) {
+          margin-left: 0.75rem;
+        }
+      `}</style>
+    </nav>
+  );
 }
 
 export default NavBar;
